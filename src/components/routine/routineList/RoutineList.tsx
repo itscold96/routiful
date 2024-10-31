56e10de58332de3cbda91347fb18dd63bd3ca727
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-31 12:18:22 +0900

    env: supabase Database type 생성

--- src/components/routine/routineList/RoutineList.tsx
+++ src/components/routine/routineList/RoutineList.tsx
@@ -1,6 +1,8 @@
 import { useRoutineList } from 'queries/useRoutineList';
+import { Link } from 'react-router-dom';
 
 export default function RoutineList() {
-  const { data, error } = useRoutineList();
+  const { data: RoutineList, error } = useRoutineList();
+
   return <div>RoutineList</div>;
 }
--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
+import { Database } from 'types/database.types';
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL as string;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY as string;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
