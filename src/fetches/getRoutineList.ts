d5e95fd92e464b03e22a5fcb8aeadc6065307c2c
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-31 12:28:52 +0900

    fix: supabase api에서 에러를 따로 반환하지 않도록 수정
    사용 부에서 데이터만 꺼내어 처리하고,
    에러 발생 시 useSuspenseQuery의 isError로 처리

--- src/fetches/getRoutineList.ts
+++ src/fetches/getRoutineList.ts
@@ -1,7 +1,7 @@
 import { supabase } from 'utils/supabaseClient';
 
 export const getRoutineList = async () => {
-  const { data, error } = await supabase.from('routine_table').select('*');
+  const { data } = await supabase.from('routine_table').select('*');
 
-  return { data, error };
+  return data;
 };
