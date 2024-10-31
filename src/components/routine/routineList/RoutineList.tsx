5ef4c4e58a871c0693c05d6d7e80506635d550b7
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-31 13:02:05 +0900

    design: 루틴 리스트 구현 및 향후 운동 리스트에서도 쓰일 스타일 공용화

--- src/components/routine/routineList/RoutineList.tsx
+++ src/components/routine/routineList/RoutineList.tsx
@@ -1,8 +1,22 @@
+import S from './RoutineList.module.scss';
+import { ListVideo } from 'lucide-react';
 import { useRoutineList } from 'queries/useRoutineList';
 import { Link } from 'react-router-dom';
 
 export default function RoutineList() {
-  const { data: RoutineList, error } = useRoutineList();
+  const { data: routineList, isError } = useRoutineList();
 
-  return <div>RoutineList</div>;
+  return (
+    <div className={S.routineList}>
+      {(isError || routineList?.length === 0) && <div>만들어진 루틴이 없습니다.</div>}
+      {routineList?.map((routine) => (
+        <Link key={routine.id} className={S.itemContainer} to={`/routine/${routine.id}/?name=${routine.name}`}>
+          <div className={S.emojiWrapper}>
+            <ListVideo size={36} strokeWidth={1.8} />
+          </div>
+          <div className={S.name}>{routine.name}</div>
+        </Link>
+      ))}
+    </div>
+  );
 }
