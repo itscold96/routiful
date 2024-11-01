92d6663584db57abef9b009f8d7b9a322eea6ee6
Minchan <98478661+itscold96@users.noreply.github.com>
2024-11-01 12:12:53 +0900

    design: 운동 아이템 퍼블리싱

--- src/components/workout/workoutList/WorkoutList.tsx
+++ src/components/workout/workoutList/WorkoutList.tsx
@@ -1,9 +1,26 @@
+import S from './WorkoutList.module.scss';
+import { Dumbbell } from 'lucide-react';
 import { useWorkoutList } from 'queries/useWorkoutList';
 
 export default function WorkoutList({ routineId }: { routineId: string | undefined }) {
-  const { data } = useWorkoutList(routineId);
+  const { data: workoutList } = useWorkoutList(routineId);
 
-  console.log('data:', data);
-
-  return <div>Workout</div>;
+  return (
+    <div className={S.workoutListContainer}>
+      {workoutList?.map((workout) => (
+        <button key={workout.id} className={S.workoutItem}>
+          <div className={S.emojiWrapper}>
+            <Dumbbell className={S.icon} size={28} strokeWidth={1.8} />
+          </div>
+          <div className={S.workoutDetail}>
+            <p>{workout.name}</p>
+            <div className={S.detail}>
+              <p>sets: {workout.sets}</p>
+              <p>reps: {workout.reps}</p>
+            </div>
+          </div>
+        </button>
+      ))}
+    </div>
+  );
 }
