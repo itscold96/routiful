3d496632f72cc95f9ccdae9fb966b387a3deda77
Minchan <98478661+itscold96@users.noreply.github.com>
2024-11-21 02:05:01 +0900

    fix: useWorkoutCount의 쿼리키 조합이 useWorkoutList와 같아서 쿼리키 무효화시 버그 발생 수정

--- src/constants/queryKeys.ts
+++ src/constants/queryKeys.ts
@@ -1,4 +1,5 @@
 export const ROUTINE_LIST_QUERY_KEY = 'routine-list';
 export const WORKOUT_LIST_QUERY_KEY = 'workout-list';
+export const WORKOUT_COUNT_QUERY_KEY = 'workout-count';
 export const WORKOUT_QUERY_KEY = 'workout';
 export const SEARCH_ROUTINE_QUERY_KEY = 'search';
--- src/queries/useWorkoutCount.ts
+++ src/queries/useWorkoutCount.ts
@@ -1,7 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
-import { WORKOUT_LIST_QUERY_KEY } from 'constants/queryKeys';
+import { WORKOUT_COUNT_QUERY_KEY, WORKOUT_LIST_QUERY_KEY } from 'constants/queryKeys';
 import { getWorkoutCount } from 'fetches/getWorkoutCount';
 
 export const useWorkoutCount = (routineId: string) => {
-  return useQuery({ queryKey: [WORKOUT_LIST_QUERY_KEY, routineId], queryFn: () => getWorkoutCount(routineId) });
+  return useQuery({
+    queryKey: [WORKOUT_LIST_QUERY_KEY, WORKOUT_COUNT_QUERY_KEY, routineId],
+    queryFn: () => getWorkoutCount(routineId),
+  });
 };
