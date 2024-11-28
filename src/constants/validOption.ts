11d842602b319739009bc5abeff6a6e666839ea9
Minchan <98478661+itscold96@users.noreply.github.com>
2024-11-28 17:45:24 +0900

    fix: 비밀번호 유효성 검사 문구 수정

--- src/constants/validOption.ts
+++ src/constants/validOption.ts
@@ -1,7 +1,7 @@
 export const VALID_OPTIONS = {
   passwordPattern: {
     value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
-    message: '영문, 숫자를 포함하여 8자 이상으로 작성해주세요',
+    message: '영문, 숫자 조합으로 8자 이상 작성해주세요',
   },
   emailPattern: {
     value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
